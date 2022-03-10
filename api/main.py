39d44858ed622c776ee988013ac5763c44b7d027
mo-ri-regen <moricontb@gmail.com>
2022-03-10 23:09:37 +0900

    ✨originの設定

--- api/main.py
+++ api/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import os
+from dotenv import load_dotenv
+
 from api.router import tasks
 
 app = FastAPI()
 
-app.include_router(task.router)
+load_dotenv()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=os.environ['ORIGINS'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    max_age=60,
+)
+
+app.include_router(tasks.router)
 
 @app.get("/hello")
 async def hello():
