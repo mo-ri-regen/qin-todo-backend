c0c74d6bc5486da3c27b8ea7c8aecf905d4c6a26
mo-ri-regen <moricontb@gmail.com>
2022-03-24 23:31:30 +0900

    ✨DBに固定のデータを登録する処理を作成

--- api/router/tasks.py
+++ api/router/tasks.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 import api.schemas.task as task_schema
 import api.cruds.task as task_crud
-# from api.db import get_db
+from api.db import get_db
 
 router = APIRouter()
 
@@ -14,7 +14,7 @@ async def get_task():
 
 @router.post("/tasks", response_model=task_schema.TaskCreateResponse)
 async def create_task(
-    # task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)
+    task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)
 ):
     return await task_crud.create_task(db, **task_body.dict())
 
