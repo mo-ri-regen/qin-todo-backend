9ed69d04e91aa00316e3d1f3ef4627694923d520
mo-ri-regen <moricontb@gmail.com>
2022-03-27 19:37:32 +0900

    🩹文字列に変換するように修正

--- api/schemas/task.py
+++ api/schemas/task.py
@@ -11,7 +11,7 @@ class TaskCreate(TaskBase):
 
 class TaskCreateResponse(TaskCreate):
     task: Optional[str] = Field(None, example="クリーニングを取りに行く!!")
-    update_at:str = Field(datetime.datetime.now(), example="2021-02-03T10:01:48.206968")
+    update_at:str = Field(str(datetime.datetime.now()), example="2021-02-03T10:01:48.206968")
     class Config:
         orm_mode = True
             
