d2006f7e9526310f4de855cf5c9c7c5de98f8812
mo-ri-regen <moricontb@gmail.com>
2022-04-03 00:55:46 +0900

    ✨データベース接続、切断

--- api/db.py
+++ api/db.py
@@ -1,5 +1,10 @@
+import asyncpg
 
 async def get_db():
     async with async_session() as session:
         yield session
+async def connect_db():
+    return await asyncpg.connect(os.environ['DATABASE_URL'])
+async def disconnect_db(conn):
+    return await conn.close()
         
