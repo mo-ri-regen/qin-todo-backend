8b840f70b687c150e8ca5421cc3ab1241089842f
mo-ri-regen <moricontb@gmail.com>
2022-04-02 21:40:22 +0900

    🔥不要なコード削除

--- api/models/task.py
+++ api/models/task.py
@@ -1,5 +1,4 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
 
 # from api.db import Base
 
@@ -10,12 +9,3 @@ from sqlalchemy.orm import relationship
 #     id = Column(Integer, primary_key=True)
 #     title = Column(String(1024))
 
-    # done = relationship("Done", back_populates="task")
-
-
-# class Done(Base):
-#     __tablename__ = "dones"
-
-#     id = Column(Integer, ForeignKey("tasks.id"), primary_key=True)
-
-#     task = relationship("Task", back_populates="done")
