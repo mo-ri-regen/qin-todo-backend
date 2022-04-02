552b4429aa4d9791e914ba855f435344b734a2ae
mo-ri-regen <moricontb@gmail.com>
2022-04-02 21:54:53 +0900

    👽️DBに登録する列を記載

--- api/models/task.py
+++ api/models/task.py
@@ -1,11 +1,13 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from api.db import Base
 
-# from api.db import Base
+class Task(Base):
+    __tablename__ = "tasks"
 
+    id = Column(Integer, primary_key=True)
+    sort_key = Column(Integer)
+    task = Column(String(1024))
 
-# class Task(Base):
-    # __tablename__ = "tasks"
-
-#     id = Column(Integer, primary_key=True)
-#     title = Column(String(1024))
-
+    due_date = Column(DateTime)
+    complete_date = Column(DateTime)
+    is_done = Column(Boolean)
