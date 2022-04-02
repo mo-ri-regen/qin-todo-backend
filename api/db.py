cb8e1800bbea97027ab03a2992a1aec61e10c8f3
mo-ri-regen <moricontb@gmail.com>
2022-04-03 00:58:43 +0900

    🚧WIP

--- api/db.py
+++ api/db.py
@@ -1,10 +1,24 @@
 import asyncpg
+# from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 
-async def get_db():
-    async with async_session() as session:
-        yield session
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# async_engine = create_async_engine(os.environ['DATABASE_URL'], echo=True)
+# async_session = sessionmaker(
+#     autocommit=False, autoflush=False, bind=connect_db.async_engine, class_=AsyncSession
+# )
+Base = declarative_base()
+
 async def connect_db():
     return await asyncpg.connect(os.environ['DATABASE_URL'])
 async def disconnect_db(conn):
     return await conn.close()
+# async def get_db():
+#     async with async_session() as session:
+#         yield session
         
--- api/router/tasks.py
+++ api/router/tasks.py
@@ -4,7 +4,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 import api.schemas.task as task_schema
 import api.cruds.task as task_crud
-from api.db import get_db
+
+from api.db import connect_db
+
 
 router = APIRouter()
 
@@ -14,10 +16,10 @@ async def get_task():
 
 @router.post("/tasks", response_model=task_schema.TaskCreateResponse)
 async def create_task(
-    task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)
+    # task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)
+    task_body: task_schema.TaskCreate, db: AsyncSession = Depends(connect_db)
 ):
-    return await task_crud.create_task(db, **task_body.dict())
-
+    return await task_crud.create_task(db, task_body)
 @router.put("/todo/{id}")
 async def edit_task():
     pass
