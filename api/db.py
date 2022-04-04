e00ba9e372760581c13811cc5fdb6733513e3476
mo-ri-regen <moricontb@gmail.com>
2022-04-04 23:17:16 +0900

    👔セッションの設定

--- api/db.py
+++ api/db.py
@@ -1,22 +1,27 @@
 import asyncpg
-# from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session, declarative_base
 
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# async_session = sessionmaker(
-#     autocommit=False, autoflush=False, bind=connect_db.async_engine, class_=AsyncSession
-# )
 ASYNC_DB_URL = (os.environ['DATABASE_URL']).replace('postgresql', 'postgresql+asyncpg')
 async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
+async_session = scoped_session(
+        sessionmaker(
+            autocommit=False
+            ,autoflush=False
+            ,
+            bind=async_engine,
+            class_=AsyncSession
+))
 Base = declarative_base()
 
 async def connect_db():
-    return await asyncpg.connect(os.environ['DATABASE_URL'])
+    return await asyncpg.connect(ASYNC_DB_URL)
 async def disconnect_db(conn):
     return await conn.close()
 # async def get_db():
