a8b57bb68b54e953ec6d6fa16d6abaf9a35595ae
mo-ri-regen <moricontb@gmail.com>
2022-04-04 23:11:59 +0900

    🐛UUIDに変更

--- api/models/task.py
+++ api/models/task.py
@@ -1,10 +1,13 @@
 from sqlalchemy import Column, Integer, String, DateTime, Boolean
 from api.db import Base
 
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+
 class Task(Base):
     __tablename__ = "tasks"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     sort_key = Column(Integer)
     task = Column(String(1024))
 
