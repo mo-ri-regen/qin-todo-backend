f43f2d5e88ffd58fb74591e147712d652473fc87
mo-ri-regen <moricontb@gmail.com>
2022-04-08 00:02:25 +0900

    👔接続文字列の置き換え方を変更

--- api/main.py
+++ api/main.py
@@ -8,7 +8,10 @@ from api.router import tasks
 app = FastAPI()
 
 load_dotenv()
-ASYNC_DB_URL = (os.environ['DATABASE_URL']).replace('postgresql', 'postgresql+asyncpg')
+ASYNC_DB_URL = os.environ['DATABASE_URL']
+# ASYNC_DB_URL = (os.environ['DATABASE_URL']).replace('postgresql', 'postgresql+asyncpg')
+# ASYNC_DB_URL = os.environ['DATABASE_URL']
+# ASYNC_DB_URL = ASYNC_DB_URL.replace('postgresql', 'postgresql+asyncpg')
 app.add_middleware(
     CORSMiddleware,
     allow_origins=ASYNC_DB_URL,
