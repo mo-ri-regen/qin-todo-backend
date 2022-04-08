358cb7de66fa46f2a6914b54710a71d2fa2a805f
takasaki376 <takasaki.a201070@outlook.jp>
2022-04-09 00:00:14 +0900

    uuid　models変更

--- api/models/task.py
+++ api/models/task.py
@@ -1,16 +1,16 @@
 from sqlalchemy import Column, Integer, String, DateTime, Boolean
 from api.db import Base
 
-from sqlalchemy.dialects.postgresql import UUID
 import uuid
+from sqlalchemy_utils import UUIDType
 
-def generate_uuid():
-    return str(uuid.uuid4())
+# def generate_uuid():
+#     return str(uuid.uuid4())
 
 class Task(Base):
     __tablename__ = "tasks"
 
-    uuid = Column(String, name="uuid", primary_key=True, default=generate_uuid)
+    id = Column(UUIDType(binary=False), primary_key=True, default=uuid.uuid4)
     sort_key = Column(Integer)
     task = Column(String(1024))
 
