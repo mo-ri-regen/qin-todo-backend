f5d60dd5940a4fddd18591775ad21a8217211847
mo-ri-regen <moricontb@gmail.com>
2022-04-10 20:33:41 +0900

    ♻️リファクタリング

--- api/main.py
+++ api/main.py
@@ -8,10 +8,8 @@ from api.router import tasks
 app = FastAPI()
 
 load_dotenv()
-ASYNC_DB_URL = os.environ['DATABASE_URL']
-# ASYNC_DB_URL = (os.environ['DATABASE_URL']).replace('postgresql', 'postgresql+asyncpg')
-# ASYNC_DB_URL = os.environ['DATABASE_URL']
-# ASYNC_DB_URL = ASYNC_DB_URL.replace('postgresql', 'postgresql+asyncpg')
+ASYNC_DB_URL = (os.environ['DATABASE_URL']).replace('postgresql', 'postgresql+asyncpg')
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=ASYNC_DB_URL,
