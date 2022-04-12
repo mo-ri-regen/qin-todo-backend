584f983fdf4c58add0f74108793de5083a558392
mo-ri-regen <moricontb@gmail.com>
2022-04-12 23:20:22 +0900

    ✨POST処理追加

--- api/router/tasks.py
+++ api/router/tasks.py
@@ -1,8 +1,8 @@
 # from fastapi import APIRouter
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import List
-
+from uuid import UUID
 import api.schemas.task as task_schema
 import api.cruds.task as task_crud
 from api.db import get_db
@@ -23,8 +23,14 @@ async def create_task(
 ):
     return await task_crud.create_task(db, task_body)
 @router.put("/todo/{id}")
-async def edit_task():
-    pass
+async def edit_task(
+   task_id: UUID, task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)
+):
+    task = await task_crud.get_task(db, task_id=task_id)
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    return await task_crud.update_task(db, task_body, original=task)    
 
 @router.delete("/todo/{id}")
 async def delete_task():
