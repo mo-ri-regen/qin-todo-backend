bdeb2b885abb15d04f7ce544adfd9921924b3eed
poc <qin.poclab@gmail.com>
2022-05-15 10:27:02 +0900

    herokuにmiddlewareを変更。ORIGINSを追加

--- api/main.py
+++ api/main.py
@@ -14,15 +14,14 @@ if ASYNC_DB_URL and ASYNC_DB_URL.startswith("postgres://"):
     ASYNC_DB_URL = ASYNC_DB_URL.replace("postgres://", "postgresql://", 1)
 ASYNC_DB_URL = ASYNC_DB_URL.replace('postgresql', 'postgresql+asyncpg')
 
-if not os.environ['DEVELOPMENT_KEY'] == "heroku":
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=os.environ['ORIGINS'],
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=os.environ['ORIGINS'],
 
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-        max_age=60,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    max_age=60,
     )
 
 app.include_router(tasks.router)
